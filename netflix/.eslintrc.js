61cd1b517b6572c8f28e36ec2948a920c347b345
HAE2NI <hae0_02ni@ewhain.net>
2024-11-09 20:38:14 +0900

    fix: prettier 배포 에러 해결

--- netflix/.eslintrc.js
+++ netflix/.eslintrc.js
@@ -1,14 +1,15 @@
 // .eslintrc.js
 module.exports = {
   extends: [
-    'next/core-web-vitals',
-    'plugin:@typescript-eslint/recommended', // 타입스크립트 관련 규칙 추가
-    'plugin:prettier/recommended', // Prettier와 ESLint를 함께 사용하도록 설정
+    "eslint:recommended",
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended", // 타입스크립트 관련 규칙 추가
+    "plugin:prettier/recommended", // Prettier와 ESLint를 함께 사용하도록 설정
   ],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   rules: {
     // 여기에 원하는 추가 규칙을 설정하귀,,,
-    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
   },
-}
+};
