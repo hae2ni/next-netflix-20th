b438644bb23ba7b26158f9cf09444f3d8b406381
HAE2NI <hae0_02ni@ewhain.net>
2024-11-09 21:07:06 +0900

    feat: Landing page 이후 home page 나오도록 설정

--- netflix/src/components/landing/Landing.tsx
+++ netflix/src/components/landing/Landing.tsx
@@ -1,27 +1,27 @@
-"use client";
+'use client';
 
-import Lottie from "lottie-react";
-import netflixLanding from "./json/netflixLanding.json";
-// import { useRouter } from "next/navigation";
+import Lottie from 'lottie-react';
+import netflixLanding from './json/netflixLanding.json';
+import { useRouter } from 'next/navigation';
 
 export default function Landing() {
-  // const router = useRouter();
+  const router = useRouter();
   return (
     <div
       style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100vw",
-        height: "100vh",
-        backgroundColor: "black",
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100vw',
+        height: '100vh',
+        backgroundColor: 'black',
       }}
     >
       <Lottie
         animationData={netflixLanding}
-        style={{ maxWidth: "100%" }}
+        style={{ maxWidth: '100%' }}
         loop={false}
-        // onComplete={() => router.push("/home")}
+        onComplete={() => router.push('/home')}
       />
     </div>
   );
