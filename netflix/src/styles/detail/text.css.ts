f7699ab6414e5fe919f68f5e36206c6542122f78
HAE2NI <hae0_02ni@ewhain.net>
2024-11-14 11:53:46 +0900

    etc: style 파일

--- netflix/src/styles/detail/text.css.ts
+++ netflix/src/styles/detail/text.css.ts
@@ -1,9 +1,13 @@
 import { style } from "@vanilla-extract/css";
 
 export const title = style({
-  color: "#FFFFFFF",
+  color: "#FFFFFF",
+  fontWeight: 700,
+  fontSize: "26.75px",
 });
 
 export const summary = style({
-  color: "#FFFFFFF",
+  color: "#FFFFFF",
+  fontWeight: 400,
+  fontSize: "11.14px",
 });
