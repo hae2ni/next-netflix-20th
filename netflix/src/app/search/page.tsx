db3dc111b1efa8d05e1f1fa3d8f7fb669932161c
HAE2NI <hae0_02ni@ewhain.net>
2024-11-14 11:54:47 +0900

    feat: detail page

--- netflix/src/app/search/page.tsx
+++ netflix/src/app/search/page.tsx
@@ -2,7 +2,6 @@ import NavBar from "@/components/common/navBar";
 import { ContentList } from "@/components/search/ContentList";
 import { SearchBar } from "@/components/search/SearchBar";
 import { page } from "@/styles/globalTheme.css";
-import { InferGetServerSidePropsType } from "next";
 
 export default function Search() {
   return (
--- netflix/src/components/detail/PlayButton.tsx
+++ netflix/src/components/detail/PlayButton.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import * as styles from "@/styles/detail/playButton.css";
 import playbtn from "/public/svgs/detail/playbutton.svg";
+import Image from "next/image";
 
 export const PlayButton = () => {
   return (
     <div className={styles.container}>
-      <img src={playbtn} alt="재생하기" />
+      <Image width={72} height={30} src={playbtn} alt="재생하기" />
     </div>
   );
 };
