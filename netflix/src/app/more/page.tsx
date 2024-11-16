ac9a0164770c898cb87c6d0108fbede109fab278
HAE2NI <hae0_02ni@ewhain.net>
2024-11-16 20:56:02 +0900

    fix: fix deploy bug

--- netflix/src/app/more/page.tsx
+++ netflix/src/app/more/page.tsx
@@ -1,12 +1,10 @@
 import NavBar from "@/components/common/navBar";
 import { PlayButton } from "@/components/detail/PlayButton";
-import { Text } from "@/components/detail/Text";
 
 export default function More() {
   return (
     <div>
       <PlayButton />
-      <Text />
       <NavBar />
     </div>
   );
